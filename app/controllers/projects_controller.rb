ecfa3d50056406632094889e3ca989d9d9cc986a
YNFYNYTY <www.rog_drummer@hotmail.com>
2014-08-18 12:11:09 +0200

    Added create, new ,and show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,31 @@
 class ProjectsController < ApplicationController
-	def  home
+	def  index
 		 @projects = Project.all
 		render 'home'
 	end
+
+	def show
+		@project = Project.find(params[:id])
+		render
+	end
+
+	def new
+    @project = Project.new
+ 	end
+
+	def create
+		@project = Project.new(project_params)
+		if @project.save
+				redirect_to projects_path notice: 'Project was successfully created.'
+		else
+			 render 'index'
+		end
+	end
+
+private
+
+	def project_params
+      params[:project].permit(:name, :description)
+ 	end
+
 end
